df02ca2871052da6b9c2570a32af5379d0b12e42
Esti Lurie <estilurie@gmail.com>
2025-07-05 15:57:59 -0400

    calendar

---
+++ src/routes/calendar/+page.svelte
@@ -0,0 +1,43 @@
+<article>
+	<h1>Upcoming</h1>
+	<ul>
+		<li class="mystical">Moshiach arrives</li>
+		<li>...</li>
+		<li>Sat 8/2/25 ~ Erev Tisha B'Av</li>
+		<li>Fri 7/11/25 ~ Shabbos @ Esti</li>
+	</ul>
+
+	<h1>Past</h1>
+	<ul>
+		<li>Tue 6/24/25 ~ Election Day (update: Zohran won!)</li>
+		<li>Fri 6/20/25 ~ Shabbos @ Esti</li>
+		<li>Sat 6/14/25 ~ Primary Early Voting begins</li>
+		<li>Sun 6/1/25 ~ Erev Shavuot</li>
+		<li>Sun 5/18/25 ~ TBRS for Zohran Canvassing</li>
+		<li>Fri 5/16/25 ~ Shabbos @ Esti</li>
+		<li>Thu 5/15/25 ~ Lag BaOmer Jackson Heights (Contact: Sara F)</li>
+		<li>Fri 4/18/25 ~ Shabbos/Seder (!!) @ Esti</li>
+		<li>Sat 4/12/25 ~ Pesach begins</li>
+		<li>Fri 3/21/25 ~ Shabbos @ Esti</li>
+		<li>Sat 3/15/25 ~ Kemp Kinderland Purim carnival (Contact: Sandra)</li>
+		<li>
+			Thu 3/13/25 ~ Purim begins, <a
+				target="_blank"
+				href="https://www.jfrej.org/events/2025/01/jfrej-purim-project-%CC%B6e%CC%B6s%CC%B6t%CC%B6h%CC%B6e%CC%B6r%CC%B6-vashti"
+				>JFREJ full megillah reading</a
+			> (Contact: Sara F)
+		</li>
+		<li>Fri 2/21/25 ~ Shabbos @ Esti</li>
+		<li>Fri 1/10/25 ~ Shabbos @ Esti</li>
+		<li>...</li>
+		<li class="mystical">
+			<a
+				href="https://www.chabad.org/library/article_cdo/aid/2641925/jewish/Which-Year-Was-the-Second-Temple-Destroyed-69-CE-or-70-CE.htm"
+				>3830 / 70 CE ~ Destruction of the Second Temple</a
+			>
+		</li>
+		<li class="mystical">3338 / 586 BCE ~ Destruction of the First Temple</li>
+		<li>...</li>
+		<li class="mystical">Creation of the universe</li>
+	</ul>
+</article>
