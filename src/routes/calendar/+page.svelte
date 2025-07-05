a5cec87d6e44fdc78438ad6461b20bf2ba1f8ce0
Esti Lurie <estilurie@gmail.com>
2025-07-05 19:13:58 -0400

    Add upcoming holidays

--- src/routes/calendar/+page.svelte
+++ src/routes/calendar/+page.svelte
@@ -3,8 +3,16 @@
 	<ul>
 		<li class="mystical">Moshiach arrives</li>
 		<li>...</li>
+		<li>Sun 12/14/25 ~ Erev Hanukkah</li>
+		<li>Tue 10/14/25 ~ Erev Simchat Torah</li>
+		<li>Mon 10/13/25 ~ Erev Shemini Atzeret</li>
+		<li>Mon 10/6/25 ~ Erev Sukkot</li>
+		<li>Wed 10/1/25 ~ Erev Yom Kippur</li>
+		<li>Mon 9/22/25 ~ Erev Rosh Hashana</li>
+		<li><strong>Fri 8/15/25 ~ Shabbos @ Esti</strong></li>
+		<li>Sat 8/9/25 ~ Tu B'Av</li>
 		<li>Sat 8/2/25 ~ Erev Tisha B'Av</li>
-		<li>Fri 7/11/25 ~ Shabbos @ Esti</li>
+		<li><strong>Fri 7/11/25 ~ Shabbos @ Esti</strong></li>
 	</ul>
 </div>
 
@@ -12,15 +20,15 @@
 <div class="calendar-card">
 	<ul>
 		<li>Tue 6/24/25 ~ Election Day (update: Zohran won!)</li>
-		<li>Fri 6/20/25 ~ Shabbos @ Esti</li>
+		<li><strong>Fri 6/20/25 ~ Shabbos @ Esti</strong></li>
 		<li>Sat 6/14/25 ~ Primary Early Voting begins</li>
 		<li>Sun 6/1/25 ~ Erev Shavuot</li>
 		<li>Sun 5/18/25 ~ TBRS for Zohran Canvassing</li>
-		<li>Fri 5/16/25 ~ Shabbos @ Esti</li>
+		<li><strong>Fri 5/16/25 ~ Shabbos @ Esti</strong></li>
 		<li>Thu 5/15/25 ~ Lag BaOmer Jackson Heights (Contact: Sara F)</li>
-		<li>Fri 4/18/25 ~ Shabbos/Seder (!!) @ Esti</li>
+		<li><strong>Fri 4/18/25 ~ Shabbos/Seder (!!) @ Esti</strong></li>
 		<li>Sat 4/12/25 ~ Pesach begins</li>
-		<li>Fri 3/21/25 ~ Shabbos @ Esti</li>
+		<li><strong>Fri 3/21/25 ~ Shabbos @ Esti</strong></li>
 		<li>Sat 3/15/25 ~ Kemp Kinderland Purim carnival (Contact: Sandra)</li>
 		<li>
 			Thu 3/13/25 ~ Purim begins, <a
@@ -29,16 +37,16 @@
 				>JFREJ full megillah reading</a
 			> (Contact: Sara F)
 		</li>
-		<li>Fri 2/21/25 ~ Shabbos @ Esti</li>
-		<li>Fri 1/10/25 ~ Shabbos @ Esti</li>
+		<li><strong>Fri 2/21/25 ~ Shabbos @ Esti</strong></li>
+		<li><strong>Fri 1/10/25 ~ Shabbos @ Esti</strong></li>
 		<li>...</li>
 		<li class="mystical">
 			<a
 				href="https://www.chabad.org/library/article_cdo/aid/2641925/jewish/Which-Year-Was-the-Second-Temple-Destroyed-69-CE-or-70-CE.htm"
-				>3830 / 70 CE ~ Destruction of the Second Temple</a
+				>9 Av 3830 ~ Destruction of the Second Temple</a
 			>
 		</li>
-		<li class="mystical">3338 / 586 BCE ~ Destruction of the First Temple</li>
+		<li class="mystical">9 Av 3338 ~ Destruction of the First Temple</li>
 		<li>...</li>
 		<li class="mystical">Creation of the universe</li>
 	</ul>
