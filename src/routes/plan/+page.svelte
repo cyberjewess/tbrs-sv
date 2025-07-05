4ebfc16ff258f1ca24e03fb078516185acab093d
Esti Lurie <estilurie@gmail.com>
2025-07-05 15:58:33 -0400

    redirect pages

---
+++ src/routes/plan/+page.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		// Redirect to Google Spreadsheet after a short delay
+		setTimeout(() => {
+			window.location.href =
+				'https://docs.google.com/spreadsheets/d/1PEzuOksZuw5YVHXDJf7OiwIw_MR50KEAxs-piA1-wvg/edit?usp=sharing';
+		}, 1000);
+	});
+</script>
+
+<div class="redirect-page">
+	<a
+		class="redirect-link text-tbrs-white font-sans text-6xl no-underline"
+		target="_blank"
+		href="https://docs.google.com/spreadsheets/d/1PEzuOksZuw5YVHXDJf7OiwIw_MR50KEAxs-piA1-wvg/edit?usp=sharing"
+	>
+		<h3>Click if not automatically redirected</h3>
+		<h3>Shabbos Potluck Planning July 11</h3>
+	</a>
+</div>
---
+++ src/routes/rsvp/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		// Redirect to Google Forms after a short delay
+		setTimeout(() => {
+			window.location.href = 'https://forms.gle/78dyQi6JWapuc8f37';
+		}, 1000);
+	});
+</script>
+
+<div class="redirect-page">
+	<a class="redirect-link" target="_blank" href="https://forms.gle/78dyQi6JWapuc8f37">
+		<h3>Click if not automatically redirected</h3>
+		<h3>Shabbos RSVP July 11</h3>
+	</a>
+</div>
