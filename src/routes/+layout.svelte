b305118b7c898bc68743a73f1574374325c2e136
Esti Lurie <estilurie@gmail.com>
2025-07-05 15:57:26 -0400

    navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,4 +4,13 @@
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div class="navbar">
+	<button><a href="/">Home</a></button>
+	<button><a href="/archive/">Archive</a></button>
+	<button><a href="/calendar/">Calendar</a></button>
+	<button><a href="/nusach/">Nusach</a></button>
+</div>
+
+<main>
+	{@render children()}
+</main>
