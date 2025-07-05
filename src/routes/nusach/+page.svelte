5d60850ec671c60ccb30448b711ef0c21c79f796
Esti Lurie <estilurie@gmail.com>
2025-07-05 15:58:11 -0400

    nusach

---
+++ src/routes/nusach/+page.svelte
@@ -0,0 +1,57 @@
+<div class="home">
+	<h1>Nusach</h1>
+	<p>Nusach is the songs we sing, the tunes and melodies of our community.</p>
+
+	<h2>
+		Contribute ~ <a target="_blank" href="https://forms.gle/vUDa6tcdjm4b3Gop6"
+			>Share a song to add here!</a
+		>
+	</h2>
+
+	<h2>Kabbalat Shabbat</h2>
+	<ul>
+		<li>
+			<a
+				href="https://youtube.com/playlist?list=PLRvPfQSPuBL9MnDQ23H1reE8LQfVS_BnS&si=bsZAz2S9AC18CL"
+			>
+				Starter Pack Playlist
+			</a>
+		</li>
+		<li>
+			<a target="_blank" href="https://youtu.be/wHF-4ElIf_Q"> L'chu N'ran'na - Joey Weisenberg </a>
+		</li>
+	</ul>
+
+	<h2>Other Shabbat</h2>
+	<ul>
+		<li>
+			<a
+				target="_blank"
+				href="https://www.youtube.com/watch?v=2yEgua8bgok&list=OLAK5uy_kaJSFWYhZ7MT-gQhgIOFI3srXzsbJg3nc&index=8"
+				>Dror Yikra - Joey Weisenberg</a
+			>
+		</li>
+	</ul>
+
+	<h2>Nigunim</h2>
+	<ul>
+		<li>Please submit a song to get it started!</li>
+	</ul>
+
+	<h2>Yiddish</h2>
+	<ul>
+		<li>
+			<a
+				target="_blank"
+				href="https://defendinghistory.com/yiddish-passover-songs-versions-from-the-vilna-area?emci=38c7afbd-dd16-f011-8b3d-0022482a9fb7&emdi=56358e2b-ed16-f011-8b3d-0022482a9fb7&ceid=4563281"
+			>
+				Peysekh Lider
+			</a>
+		</li>
+	</ul>
+
+	<h2>Other</h2>
+	<ul>
+		<li><a href="./nirtzah.pdf">Pesach Seder - Nirtzah</a></li>
+	</ul>
+</div>
