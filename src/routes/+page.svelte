c0b94f23dcfe8e0b015e1cd19009eaf3a6611ab1
Esti Lurie <estilurie@gmail.com>
2025-07-05 15:57:52 -0400

    homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="home">
+	<h1>You are at the Bushwick / Ridgewood Shul.</h1>
+
+	<div class="upcoming">
+		<h2>Upcoming: Shabbos @ Esti on Friday, July 11.</h2>
+		<h3 class="event-link">
+			<a target="_blank" href="/rsvp"> RSVP Form </a>
+		</h3>
+		<h3 class="event-link">
+			<a target="_blank" href="/plan"> Potluck Spreadsheet </a>
+		</h3>
+		<h2>Services 6:30pm. Potluck dinner 7:30pm.</h2>
+	</div>
+
+	<div class="principles">
+		An intergenerational Jewish community.
+		<br />
+		<br />
+		We are Liberationist Diasporist Anarchist <strong>dreamers.</strong>
+		<br />
+		<br />
+		We are Anti-Zionist Anti-Implerialist Anti-Colonialist <strong>fighters.</strong>
+		<br />
+		<br />
+		We do monthly trad-egal style Shabbos services <strong>with a potluck dinner.</strong>
+		<br />
+		<br />
+		In order to rest, renew, and connect.
+		<br />
+		<br />
+	</div>
+
+	<div class="footnote">
+		Yes, these colors are lifted straight off of the Zohran for Mayor website.
+	</div>
+</div>
