40e6c6d43cacf1fe04de032370de54645bd50829
Esti Lurie <estilurie@gmail.com>
2025-07-09 22:27:59 -0400

    Change name to democracy

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,7 @@
 
 <div class="navbar">
 	<a href="/" class="button">Home</a>
-	<a href="/vote/" class="button">Vote</a>
+	<a href="/democracy/" class="button">Democracy</a>
 	<a href="/calendar/" class="button">Calendar</a>
 	<a href="/nusach/" class="button">Nusach</a>
 	<a href="/archive/" class="button">Archive</a>
---
+++ src/routes/democracy/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	const form = {
+		link: 'https://forms.gle/78Dpnps5FQD985an7',
+		display: 'Name our community!'
+	};
+</script>
+
+<h1>Suggest</h1>
+<p class="centered">Put it in the box.</p>
+<div class="vote-card">
+	<ul>
+		<li>
+			<a href={form.link} class="button">
+				<pre>{form.display}</pre>
+			</a>
+		</li>
+	</ul>
+</div>
+
+<h1>Vote</h1>
+<p class="centered">Like your life depends on it.</p>
+<div class="vote-card">
+	<ul>
+		<li>
+			<h4>Naming vote will be in August.</h4>
+		</li>
+	</ul>
+</div>
