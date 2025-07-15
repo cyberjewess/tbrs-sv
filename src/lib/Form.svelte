7f6ab8384d9a93a829d024e850fc84e648c1d6c7
Esti Lurie <estilurie@gmail.com>
2025-07-14 22:23:23 -0400

    Add Form WIP

---
+++ src/lib/Form.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import type { EventHandler } from 'svelte/elements';
+	import { todayISO } from '$lib/data/events';
+
+	const extractFormData = (event) => {
+		const data = new FormData(event.currentTarget);
+		return Object.fromEntries(data.entries());
+	};
+
+	const onSubmit: EventHandler = (event) => {
+		console.log(extractFormData(event));
+	};
+
+	let text = '...';
+	const onClick = () => {
+		text = '';
+	};
+</script>
+
+<form onsubmit={onSubmit}>
+	<input name="date" value={todayISO()} type="date" />
+	<input name="title" value={text} onclick={onClick} type="text" />
+	<button type="submit">Submit</button>
+</form>
---
+++ src/routes/events/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import Form from '$lib/Form.svelte';
+</script>
+
+<h1>New Event</h1>
+<div class="calendar-card">
+	<Form />
+</div>
