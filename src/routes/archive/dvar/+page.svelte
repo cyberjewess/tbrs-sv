5a09e6ba9ee1d59ff1ce3f502fe061e3bc52e3fa
Esti L <estilurie@gmail.com>
2025-07-19 10:22:36 -0400

    Link to abigail’s drash

--- src/routes/archive/dvar/+page.svelte
+++ src/routes/archive/dvar/+page.svelte
@@ -1,6 +1,9 @@
 <div class="home">
 	<h1>Divrei Torah</h1>
 	<ul>
+<li>
+<h2><a href="/">July 11, 2025 - Parshat Balak, Abigail D</a></h2>
+</li>
 		<li>
 			<h2><a href="/archive/dvar/2025-06-20/">June 20, 2025 - Parshat Shlakh, Daniel W</a></h2>
 		</li>
