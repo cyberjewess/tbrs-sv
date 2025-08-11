e0d2d8386fd2fc138e07ec9dfd220b541eb03d00
Esti Lurie <estilurie@gmail.com>
2025-08-10 21:44:23 -0400

    Add name page

---
+++ src/routes/name/+page.svelte
@@ -0,0 +1,95 @@
+<div class="home">
+	<h1 class="centered">Name descriptions & explanations</h1>
+	<h1>
+		<a href="https://www.rcv123.org/ballot/JQXcpJqf6ww58vJJp3hqr4xC"
+			>Find the ballot here! Voting closes August 15 at 5PM.</a
+		>
+	</h1>
+	<h3>Solarpunk Shul</h3>
+	<h3>Kol Yisroel (Voice of Israel)</h3>
+	<h3>Darkhei Tzedek (Paths/Ways of Justice)</h3>
+	<h3>Shtibl Ridgewick</h3>
+	<h3>The Bushwick Ridgewood Shul</h3>
+	<ul>
+		<li>Suggested by Avra</li>
+		<li>Continuity! I like this name lol</li>
+	</ul>
+	<h3>Congregation Agudat Yisroel</h3>
+	<ul>
+		<li>Suggested by Esti</li>
+		<li>The name adorning the synagogue at 1618 Cornelia Street.</li>
+	</ul>
+	<h3>Tefilat HaDerech - תפילת הדרך (The Traveler's Prayer)</h3>
+	<ul><li>Because we are diasporist border-crossers on the move towards liberation!</li></ul>
+	<h3>Tiferet Beit Rodfei Shalom (The Glorious House of the Pursuer's of Peace)</h3>
+	<ul>
+		<li>Suggested by Caroline & Paul</li>
+		<li>
+			"Keeps the TBRS acronym." "I like the nod to TBRS' original name, as well as Rabbis 4
+			Ceasefire."
+		</li>
+	</ul>
+	<h3>
+		Kehilat Ohev Tzdakah U'Mishpat - קהילת אוהב צדקה ומשפט (The Community that Loves Justice and
+		Law)
+	</h3>
+	<ul>
+		<li>Suggested by Yisroel</li>
+		<li>
+			<p>
+				דאס שטאמט פון שמונה עשרה און איך מיין אז סאיז א גוטע נאמען וואס רעפרעסענטירט זייער גוט
+				פארוואס מיר דאווענען ביי אט דעם שול אנשטאט קיין אנדערען. איך האב ליב אויך שטיבל רידזשוויק
+				אבער איך האב געוואלט איינווארפן מיין היטל אינעם רינג.
+			</p>
+			<p>
+				This comes from the Shmoneh Esreh (the Amidah) and I think it's a good name that represents
+				very well why we daven at this shul and not any other. I also love Shtibl Ridgewick but I
+				wanted throw my hat in the ring. (translated by Esti)
+			</p>
+		</li>
+	</ul>
+
+	<h3>Beth Kherut Spinoza - בית חירות שפינוזה</h3>
+	<ul>
+		<li>Suggested by Alaska</li>
+		<li>
+			<p>
+				Brauch Spinoza was a noted Jewish intellectual, dissident, and philosopher who
+				excommunicated from the Sephardic Jewish community in Amsterdam that he grew up in on July
+				27th, 1656, due to vocal disagreements he had with the local Jewish community that the
+				rabbinical establishment of Amsterdam declared as "abominable heresies", as they felt his
+				opinions were threatening to the narratives that where using for religious and social
+				cohesion within. His expulsion came about for a variety of factors, but amongst the most
+				important was due to his belief that the Torah was not divinely gifted in its entirely on
+				mount Saini, and instead was authored by humans, as well as rejecting the concept of a
+				personal god, as well as refuting ideas regarding the unique chosenness of Jews in the eyes
+				of God.
+			</p>
+			<p>
+				Spinoza's beliefs are not dissimilar modern day humanistic Judaism and where considered
+				highly controversial in his Sephardic community that at the time was facing a large influx
+				of refugee Jews fleeing the Spanish inquisition, leading to an increase in political
+				instability within the region, thus eliciting a harsher response by community leaders to
+				Spinoza's dissenting theological opinions.
+			</p>
+			<p>
+				All of this of course, begs the question as to why it would make sense to name a shul after
+				a man noted as a heretic by his own community during his lifetime. My agreement for this is
+				that in using Spinoza in our name, we can, in the way that this shul refers to itself,
+				embody a similar spirit of dissent from current Jewish cultural norms regarding Zionism, a
+				dissent that in many ways similar to Spinoza's, as in both cases our dissent alienates us
+				from much of the mainstream Jewish community, but will likely retrospectively be considered
+				an important movement within Judaism.
+			</p>
+			<p>
+				In particular, I think there is also a degree of metaphorical alignment between Spinoza's
+				premise that Jewish people, and humans more broadly, are not specifically privileged in the
+				eyes of God, and anti-Zionism, which in its modern for is in many ways is derived from the
+				premise that Jews are not more entitled to exist in the land of Palestine any more than the
+				Palestinians who already lived there. In addition to this, Spinoza greatly advocated for a
+				focus on ethics and living an ethical life, which is another important alignment his views
+				have with anti-Zionism.
+			</p>
+		</li>
+	</ul>
+</div>
